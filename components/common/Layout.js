92cd607c26a31960e6218912b6add9138b7bd292
lucas jin <lucas1004jx@gmail.com>
2019-09-27 09:35:38 +0200

    ass logo svg asn style

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../../styles/common/general.scss';
+import '../../styles/common/layout.scss';
 
 const Layout = ({ children }) => {
   return (
     <div>
-      logo
+      <img src="/static/svg/LOGO.svg" alt="logo" className="logo" />
       {children}
     </div>
   );
