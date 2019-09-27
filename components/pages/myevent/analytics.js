553dcbc01346dcdfceb7f2208a8b110496aede44
lucas jin <lucas1004jx@gmail.com>
2019-09-27 16:08:15 +0200

    adjust style

--- components/pages/myevent/analytics.js
+++ components/pages/myevent/analytics.js
@@ -4,12 +4,22 @@ const Analytics = () => {
 
   return (
     <div className="analyticsInfo info">
-      <figure>
-        <img src="/static/img/analytics01.png" alt="analytics" />
-      </figure>
-      <figure>
-        <img src="/static/img/analytics02.png" alt="analytics" />
-      </figure>
+      <h2>Analytics</h2>
+      <div className="r1">
+        <figure>
+          <img src="/static/img/analytics01.png" alt="analytics" />
+        </figure>
+      </div>
+      
+      <div className="r2">
+        <figure>
+          <img src="/static/img/analytics02.png" alt="analytics" />
+        </figure>
+        <figure>
+          <img src="/static/img/analytics03.png" alt="analytics" />
+        </figure>
+      </div>
+      
     </div>
   );
 };
